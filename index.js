c0ec4b8fb7f6de055d20d5eca8e771a956f7991d
Vladislav Hristov <vlad@Vladislavs-Mac-mini.local>
2018-06-20 14:08:31 +0300

    add predefined orders

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const Client = require('./lib/Client');
+const Orders = require('./lib/predefinedOrders');
 const fsKeysStorage = require('./lib/storages/fsKeysStorage');
 
 module.exports = {
 	Client,
+	Orders,
 	fsKeysStorage,
 };

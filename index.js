945499290a8698aed504b573019de2c23148006a
Vladislav Hristov <vlad@Vladislavs-Mac-mini.local>
2018-06-20 12:20:03 +0300

    client and order optimization

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 'use strict';
 
 const Client = require('./lib/Client');
-const OrderBuilder = require('./lib/OrderBuilder');
-const ISO20022Builder = require('./lib/ISO20022OrderBuilder');
-const keysManager = require('./lib/keymanagers/keysManager');
-const fsKeysStorage = require('./lib/keymanagers/fsKeysStorage');
+const fsKeysStorage = require('./lib/storages/fsKeysStorage');
 
 module.exports = {
 	Client,
-	OrderBuilder,
-	ISO20022Builder,
-	keysManager,
 	fsKeysStorage,
 };

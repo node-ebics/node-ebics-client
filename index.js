548c17a868b4edcd894ce53f20435e52d45cdda3
Vladislav Hristov <vlad@Vladislavs-Mac-mini.local>
2018-08-30 13:53:24 +0300

    Add serialize function to BankLetter class

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const Client = require('./lib/Client');
 const Orders = require('./lib/predefinedOrders');
 const fsKeysStorage = require('./lib/storages/fsKeysStorage');
+const BankLetter = require('./lib/BankLetter');
 
 module.exports = {
 	Client,
 	Orders,
+	BankLetter,
 	fsKeysStorage,
 };

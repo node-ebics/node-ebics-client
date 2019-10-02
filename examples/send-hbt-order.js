166c61aec4a247d923de82278271ec02cbef815f
Yago Perez <yagop@users.noreply.github.com>
2019-10-02 23:27:23 +0200

    examples/send-hbt-order.js

---
+++ examples/send-hbt-order.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const ebics = require('../index');
+
+const client = new ebics.Client({
+	url: 'https://ebics.server',
+	partnerId: '',
+	userId: '',
+	hostId: '',
+	passphrase: 'test', // keys-test will be decrypted with this passphrase
+	keyStorage: ebics.fsKeysStorage('./keys-test'),
+});
+
+// Client keys must be already generated and send by letter.
+// The bank should have enabled the user
+client.send(ebics.Orders.HTD)
+	.then((resp) => {
+		console.log('Respose for HTD order %j', resp);
+		if (resp.technicalCode !== '000000')
+			throw new Error('Something went wrong');
+
+		const data = Buffer.from(resp.orderData);
+		console.log(data.toString('utf8'));
+	})
+	.catch((err) => {
+		console.error(err);
+		process.exit(1);
+	});
